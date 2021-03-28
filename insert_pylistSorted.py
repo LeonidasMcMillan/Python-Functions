d2bbed8aa53e6baaa986c14269d0c963ce0b52a8
Leonidas <54154959+LeonidasMcMillan@users.noreply.github.com>
2021-03-27 23:18:56 -0700

    Update insert_pylistSorted.py
    Needed some updating for the empty list case

--- insert_pylistSorted.py
+++ insert_pylistSorted.py
@@ -1,10 +1,13 @@
 def insert(inputList, n):
-    if n > inputList[len(inputList)-1]:
-        inputList.append(n)
-        return inputList
-    for i in range(len(inputList)):
-        if inputList[i] > n:
-            index = i
-            break
-    result = inputList[: i] + [n] + inputList[i :]
-    return result
+	if len(inputList) == 0:
+		inputList.append(n)
+		return inputList
+	if n > inputList[len(inputList)-1]:
+		inputList.append(n)
+		return inputList
+	for i in range(len(inputList)):
+    	if inputList[i] > n:
+        	index = i
+        	break
+	result = inputList[: i] + [n] + inputList[i :]
+	return result
